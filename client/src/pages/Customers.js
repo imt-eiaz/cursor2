28a69a54ec3920376f8e5dbadd7edef8892bce31
imt-eiaz <imteiaz@gmail.com>
2025-08-24 10:02:02 +0100

    Dropdown menu added

--- client/src/pages/Customers.js
+++ client/src/pages/Customers.js
@@ -340,6 +340,48 @@ const Customers = () => {
                   />
                 </div>
 
+                <div className="mb-3">
+                  <label htmlFor="mobileBrand" className="form-label">
+                    Select your mobile brand :{" "}
+                  </label>
+                  <select
+                    className="form-select"
+                    aria-label="Choose Mobile Brand"
+                    id="mobileBrand"
+                    // onChange={(e) => setBrand(e.target.value)}
+                  >
+                    <option selected>Choose Brand</option>
+                    <option value="Apple">Apple</option>
+                    <option value="Samsung">Samsung</option>
+                    <option value="Nothing">Nothing</option>
+                    <option value="Google">Google</option>
+                    <option value="Oneplus">Oneplus</option>
+                    <option value="Vivo">Vivo</option>
+                    <option value="iQOO">iQOO</option>
+                    <option value="Realme">Realme</option>
+                    <option value="Oppo">Oppo</option>
+                    <option value="Poco">Poco</option>
+                    <option value="Asus">Asus</option>
+                    <option value="Micromax">Micromax</option>
+                    <option value="Xiaomi">Xiaomi</option>
+                    <option value="Motorola">Motorola</option>
+                  </select>
+                </div>
+
+                <div className="mb-3">
+                  <label htmlFor="modelName" className="form-label">
+                    Enter Model Name :{" "}
+                  </label>
+                  <input
+                    type="text"
+                    name="modelName"
+                    id="modelName"
+                    className="input-field mt-1"
+
+                    // onChange={(e) => setModel(e.target.value)}
+                  />
+                </div>
+
                 <div>
                   <label className="block text-sm font-medium text-gray-700">
                     Status
@@ -352,6 +394,73 @@ const Customers = () => {
                     }
                     className="input-field mt-1"
                   />
+                </div>
+
+                <div className="mb-3">
+                  <label htmlFor="mobileBrand" className="form-label">
+                    Status :{" "}
+                  </label>
+                  <select
+                    className="form-select"
+                    aria-label="Choose Mobile Brand"
+                    id="mobileBrand"
+                    // onChange={(e) => setBrand(e.target.value)}
+                  >
+                    <option selected>Choose status</option>
+                    <option value="Apple">COMPLETED</option>
+                    <option value="Samsung">IN PROGRESS</option>
+                    <option value="Nothing">PENDING</option>
+                    <option value="Google">REJECTED</option>
+                  </select>
+                </div>
+
+                <div className="mb-3">
+                  <label htmlFor="repairDesc" className="form-label">
+                    Description of issue :{" "}
+                  </label>
+                  <textarea
+                    className="form-control"
+                    id="repairDesc"
+                    rows="3"
+                    // onChange={(e) => setDescription(e.target.value)}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label className="form-label" htmlFor="repairType">
+                    Select your repair :{" "}
+                  </label>
+                  <select
+                    className="form-select"
+                    aria-label="Choose Repair Type"
+                    id="repairType"
+                    // onChange={(e) => setType(e.target.value)}
+                  >
+                    <option defaultValue>Choose Repair</option>
+                    <option value="Battery Replacement">
+                      Battery Replacement
+                    </option>
+                    <option value="Display + Glass Replacement">
+                      Display + Glass Replacement
+                    </option>
+                    <option value="Speaker Replacement">
+                      Speaker Replacement
+                    </option>
+                    <option value="Camera Replacement">
+                      Camera Replacement
+                    </option>
+                    <option value="Tempered Glass Replacement">
+                      Tempered Glass Replacement
+                    </option>
+                    <option value="Button Replacement">
+                      Button Replacement
+                    </option>
+                    <option value="Charging Port Replacement">
+                      Charging Port Replacement
+                    </option>
+                    <option value="Body Housing Replacement">
+                      Body Housing Replacement
+                    </option>
+                  </select>
                 </div>
 
                 <div>
