33d3b2e84516b51afb97f4568574bbeebfb68f81
imt-eiaz <imteiaz@gmail.com>
2025-08-25 23:29:45 +0100

    Navbar adjested

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -12,13 +12,13 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="bg-white shadow-sm border-b border-gray-200">
+    <nav className="bg-green-500 shadow-sm border-b border-gray-200">
       <div className="max-w-9xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex items-center">
             <div className="flex-shrink-0">
-              <h1 className="text-4xl font-bold text-green-600">
-                PhoneBox Gadgets
+              <h1 className="text-4xl font-bold text-gray-700">
+                PhoneBox Gadgets Cirencester
               </h1>
             </div>
           </div>
