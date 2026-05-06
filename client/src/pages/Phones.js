a2a476aee8b45b123751e12e418b84361b7465b6
imt-eiaz <imteiaz@gmail.com>
2026-05-06 10:01:18 +0100

    Cirencester added to Phone page

--- client/src/pages/Phones.js
+++ client/src/pages/Phones.js
@@ -166,7 +166,9 @@ const Phones = () => {
       <div className="flex justify-between items-center">
         <div>
           <h1 className="text-2xl font-bold text-gray-900">Phones</h1>
-          <p className="text-gray-600">Manage your phone records</p>
+          <p className="text-gray-600">
+            Manage your phone records "Cirencester"
+          </p>
         </div>
         <button
           onClick={openNewCustomerModal}
